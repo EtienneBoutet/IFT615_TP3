75031b3ac9a05dc12a56b6f7e86aed3515ba9669
Etienne <etienne.boutet@outlook.com>
2020-07-08 11:33:06 -0400

    cleanup numero 1

--- solution_correcteur.py
+++ solution_correcteur.py
@@ -63,38 +63,25 @@ class Correcteur:
 
         T = len(mot)
         K = self.p_transition.shape[0]
-        alpha = np.empty((K, T), dtype=object)
-        
-        for i in range(K):
-            prob = self.p_observation[self.letters2int[mot[0]], i] * self.p_init[i]
-            alpha[i][0] = (prob, None)
+        alpha = np.empty((K, T))
+        beta = np.empty((K, T))
 
-        # alpha[:, 0] = (self.p_init * self.p_observation[self.letters2int[mot[0]], :], None)
+        # Création des tables alpha et beta
+        alpha[:, 0] = self.p_init * self.p_observation[self.letters2int[mot[0]], :]
+        beta[:, 0] = 0
 
         for t in range(1, T):
             for i in range(K):
-                max = 0
-                provientDe = -1
-                for j in range(K):
-                    candidatMax = self.p_transition[i, j] * alpha[j, t-1][0]
-                    if candidatMax > max:
-                        max = candidatMax
-                        provientDe = j
+                alpha[i, t] = self.p_observation[self.letters2int[mot[t]], i] * np.max(self.p_transition[i, :] * alpha[:, t - 1])
+                beta[i, t] = np.argmax(alpha[:, t - 1] * self.p_transition[i, :])
 
-                prob = self.p_observation[self.letters2int[mot[t]], i] * max
-                alpha[i][t] = (prob, provientDe)
-
-                # alpha[i, t] = self.p_observation[self.letters2int[mot[t]], i] * np.max(alpha[:, t - 1] * self.p_transition[:, i])
-                # beta[i, t] = np.argmax(alpha[:, t - 1] * self.p_transition[:, i])
-
-
-        # Construire le résultat
+        # Construction du mot corrigé
         result = ""
         ind = np.argmax(alpha[:, T - 1])
         result += self.int2letters[int(ind)]
 
         for j in range(T - 1, 0, -1):
-            ind = alpha[int(ind), j][1]
+            ind = beta[int(ind), j]
             result += self.int2letters[int(ind)]
 
         result = result[::-1]
